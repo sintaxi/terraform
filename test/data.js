96240bc512e7c6429f08acaf54213c0bc7397e48
Brock Whitten <brock@chloi.io>
2013-04-13 16:55:22 -0700

    adding tests for data access

---
+++ test/data.js
@@ -0,0 +1,19 @@
+var should    = require('should')
+var polymer   = require('../')
+
+describe("date", function(){
+
+  var root = __dirname + "/fixtures/data"
+  var poly = polymer.root(root)
+
+  it("should be available in the layouts", function(done){
+    poly.render("index.jade", function(error, body){
+      should.not.exist(error)
+      should.exist(body)
+      body.should.include("<h1>My Articles</h1>")
+      body.should.include("<h5>Earth people, New York to California</h5>")
+      done()
+    })
+  })
+
+})
