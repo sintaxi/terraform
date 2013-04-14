fe0986c46b4c436c50203dcca1c578b6fa5b2505
Brock Whitten <brock@chloi.io>
2013-04-13 18:39:39 -0700

    adds less

--- index.js
+++ index.js
@@ -1,92 +1,81 @@
+
 var fs          = require('fs')
 var path        = require('path')
-var template    = require('./lib/template')
 var stylesheet  = require('./lib/stylesheet')
-
-var scope       = require("./lib/template/scope")
-
+var scope       = require('./lib/template/scope')
 
 // expose helpers
 exports.helpers = helpers = require('./lib/helpers')
 
 exports.root = function(root, callback){
-  
+
   var layout  = helpers.findFirstFile(root, ["_layout.jade", "_layout.html.jade"])
   var data    = helpers.dataTree(root)
-  
+
   return {
+
     render: function(filePath, locals, callback){
-      
-      /** 
-       * locals are optional
-       */
-       
+
+      // locals are optional
       if(!callback){
         callback = locals
         locals   = {}
       }
-      
-      /** 
-       * layout
-       */
-       
-      if(layout) locals['layout'] = layout
 
-      /** 
-       * current
-       */
-       
-      locals.current = helpers.getCurrent(filePath)
-      
-      
-      /** 
-       * copy over the templateLocals
-       */
-       
-      var templateLocals = helpers.walkData(locals.current.path, data)
+      // get extension
+      var ext = path.extname(filePath).replace(/^\./, '')
 
-      for(var local in templateLocals){
-        locals[local] = templateLocals[local]
+      if(["jade", "md"].indexOf(ext) !== -1) {
+
+        // layout
+        if(layout) locals['layout'] = layout
+
+        // current
+        locals.current = helpers.getCurrent(filePath)
+
+        // scope and render
+        var output = scope(root, { globals: { public: data } }).partial(filePath, locals)
+
+        // return
+        callback(null, output)
+
+      }else if(["less"].indexOf(ext) !== -1){
+        stylesheet(root, filePath, callback)
       }
 
-      /** 
-       * render file
-       */
-       
-      var output = scope(root, { globals: { public: data } }).partial(filePath, locals)
-      callback(null, output)
+
     }
   }
-  
+
 }
 
 // exports.process = function(sourcePath, options, callback){
-//   
+//
 //   // get extension
 //   var ext = path.extname(sourcePath).replace(/^\./, '')
-//   
+//
 //   var info = {
 //     sourcePath: sourcePath,
 //     sourceType: helpers.sourceType(sourcePath),
 //     outputPath: helpers.outputPath(sourcePath),
 //     outputType: helpers.outputType(sourcePath)
 //   }
-//   
+//
 //   /**
 //    * Templates
-//    */ 
+//    */
 //   if(["jade", "md"].indexOf(ext) !== -1) {
 //     var render = template
-//   
+//
 //   /**
 //    * Stylesheets
 //    */
 //   }else if(["less"].indexOf(ext) !== -1){
 //     var render = stylesheet
 //   }
-//   
+//
 //   render(sourcePath, options, function(err, output){
 //     callback(err, info, output)
 //   })
-//   
+//
 // }
--- test/less.js
+++ test/less.js
@@ -1,38 +1,29 @@
-var should    = require('should')
-var processor = require('../')
+var should  = require('should')
+var polymer = require('../')
 
 describe("less", function(){
-  
-  var root = __dirname + '/fixtures/stylesheets'
-  
-  // it("should exist", function(done){
-  //   should.exist(processor)
-  //   processor.should.have.property("process")
-  //   done()
-  // })
-  // 
-  // it("should have basic css file", function(done){
-  //   processor.process("main.less", { root: root }, function(error, info, body){
-  //     should.not.exist(error)
-  //     info.sourcePath.should.eql("main.less")
-  //     info.sourceType.should.eql("less")
-  //     info.outputPath.should.eql("main.css")
-  //     info.outputType.should.eql("css")
-  //     body.should.include("body{background:pink;}")
-  //     done()
-  //   })
-  // })
-  // 
+
+  var root = __dirname + '/fixtures/stylesheets/less'
+  var poly = polymer.root(root)
+
+  it("should have basic css file", function(done){
+    poly.render("main.less", function(error, body){
+      should.not.exist(error)
+      body.should.include("body{background:pink;}")
+      done()
+    })
+  })
+
   // it("should return errors if error found", function(done){
-  //   processor.process("invalid.less", { root: root }, function(error, info, body){
+  //   poly.render("invalid.let }, function(error, body){
   //     should.not.exist(body)
-  //     
+  //
   //     should.exist(info)
   //     info.sourcePath.should.eql("invalid.less")
   //     info.sourceType.should.eql("less")
   //     info.outputPath.should.eql("invalid.css")
   //     info.outputType.should.eql("css")
-  //     
+  //
   //     should.exist(error)
   //     error.should.have.property("name")
   //     error.should.have.property("message")
@@ -40,5 +31,5 @@ describe("less", function(){
   //     done()
   //   })
   // })
-  
+
 })
