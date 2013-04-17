df2f906492b8578d1a55e5ea0eee1a5f4564dc92
Brock Whitten <brock@chloi.io>
2013-04-17 03:44:39 -0700

    deals with missing less file

--- test/helpers.js
+++ test/helpers.js
@@ -2,9 +2,17 @@ var should    = require('should')
 var polymer   = require('../')
 
 describe("helpers", function(){
-  // TBD
-  it("should handle missing file", function(done){
+
+  it("should handle missing template file", function(done){
     polymer.root(__dirname + "/fixtures/data").render("missing.jade", function(error, body){
+      should.not.exist(error)
+      should.not.exist(body)
+      done()
+    })
+  })
+
+  it("should handle missing stylesheet file", function(done){
+    polymer.root(__dirname + "/fixtures/data").render("missing.less", function(error, body){
       should.not.exist(error)
       should.not.exist(body)
       done()
