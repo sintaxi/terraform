9715dc70e6d242316fb446b2f96aa19f66091e6a
Brock Whitten <brock@chloi.io>
2013-04-17 03:39:29 -0700

    return null when missing template

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,13 @@
 var should    = require('should')
-var processor = require('../')
+var polymer   = require('../')
 
 describe("helpers", function(){
   // TBD
+  it("should handle missing file", function(done){
+    polymer.root(__dirname + "/fixtures/data").render("missing.jade", function(error, body){
+      should.not.exist(error)
+      should.not.exist(body)
+      done()
+    })
+  })
 })
