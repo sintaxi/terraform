71feb4ee656d0f0cd7cdf3ea11186b5796070e53
Brock Whitten <brock@chloi.io>
2013-04-19 04:00:26 -0700

    adds tests for errors

---
+++ test/errors.js
@@ -0,0 +1,45 @@
+var should    = require('should')
+var polymer   = require('../')
+
+describe("data", function(){
+
+  var root = __dirname + "/fixtures/errors"
+  var poly = polymer.root(root)
+
+  it("should get errors obj from jade", function(done){
+    poly.render("novar.jade", function(error, body){
+      should.not.exist(body)
+      should.exist(error)
+      error.should.have.property('name')
+      error.should.have.property('message')
+      //error.should.have.property('path')
+      error.should.have.property('stack')
+      done()
+    })
+  })
+
+  it("should get error if var missing in less", function(done){
+    poly.render("novar.less", function(error, body){
+      should.not.exist(body)
+      should.exist(error)
+      error.should.have.property('name')
+      error.should.have.property('message')
+      //error.should.have.property('path')
+      error.should.have.property('stack')
+      done()
+    })
+  })
+
+  it("should get errors if syntax not correct", function(done){
+    poly.render("invalid.less", function(error, body){
+      should.not.exist(body)
+      should.exist(error)
+      error.should.have.property('name')
+      error.should.have.property('message')
+      //error.should.have.property('path')
+      error.should.have.property('stack')
+      done()
+    })
+  })
+
+})
