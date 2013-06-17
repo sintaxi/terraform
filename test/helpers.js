7b75ecb1bf8c894060d40e16c98364e5c83ff0e2
Brock Whitten <brock@chloi.io>
2013-06-17 02:39:45 -0700

    more tests for priority list

--- test/helpers.js
+++ test/helpers.js
@@ -3,7 +3,7 @@ var polymer   = require('../')
 
 describe("helpers", function(){
 
-  it("should handle missing template file", function(done){
+  it("should handle missing template file.", function(done){
     polymer.root(__dirname + "/fixtures/data").render("missing.jade", function(error, body){
       should.not.exist(error)
       should.not.exist(body)
@@ -11,7 +11,7 @@ describe("helpers", function(){
     })
   })
 
-  it("should handle missing stylesheet file", function(done){
+  it("should handle missing stylesheet file.", function(done){
     polymer.root(__dirname + "/fixtures/data").render("missing.less", function(error, body){
       should.not.exist(error)
       should.not.exist(body)
@@ -21,58 +21,52 @@ describe("helpers", function(){
 
   describe('priority list', function(){
 
-    it('should build priority list for html file', function(done){
+    it('should return all possible file names for html ordered by priority.', function(done){
       var list = polymer.helpers.buildPriorityList('index.html')
       list.should.be.an.instanceOf(Array)
       list.should.have.lengthOf(4)
-      list.should.include('index.jade')
-      list.should.include('index.md')
-      list.should.include('index.html.jade')
-      list.should.include('index.html.md')
+      var plist = "index.jade, index.md, index.html.jade, index.html.md".split(', ')
+      list.should.eql(plist)
       done()
     })
 
-    it('should build priority list for css file', function(done){
+    it('should build priority list for css file.', function(done){
       var list = polymer.helpers.buildPriorityList('main.css')
       list.should.be.an.instanceOf(Array)
       list.should.have.lengthOf(2)
-      list.should.include('main.less')
-      list.should.include('main.css.less')
+      list.should.eql("main.less, main.css.less".split(', '))
       done()
     })
 
-    it('should build priority list assuming template file when unknown', function(done){
+    it('should build priority list assuming template file when unknown.', function(done){
       var list = polymer.helpers.buildPriorityList('feed.xml')
       list.should.be.an.instanceOf(Array)
       list.should.have.lengthOf(2)
-      list.should.include('feed.xml.jade')
-      list.should.include('feed.xml.md')
+      list.should.eql('feed.xml.jade, feed.xml.md'. split(', '))
       done()
     })
 
-    it('should build priority list assuming template file when asking for json', function(done){
+    it('should look for templates on json files.', function(done){
       var list = polymer.helpers.buildPriorityList('profile.json')
       list.should.be.an.instanceOf(Array)
       list.should.have.lengthOf(2)
       list.should.include('profile.json.jade')
       list.should.include('profile.json.md')
+      list.should.eql('profile.json.jade, profile.json.md'. split(', '))
       done()
     })
 
-    it('should build priority list assuming template file when missing ext', function(done){
+    it('should look for templates when no ext present.', function(done){
       var list = polymer.helpers.buildPriorityList('appcache')
       list.should.be.an.instanceOf(Array)
       list.should.have.lengthOf(2)
-      list.should.include('appcache.jade')
-      list.should.include('appcache.md')
+      list.should.eql('appcache.jade, appcache.md'.split(', '))
       done()
     })
 
-    it('should know when file is right', function(done){
-      var list = polymer.helpers.buildPriorityList('index.jade')
-      list.should.be.an.instanceOf(Array)
-      list.should.have.lengthOf(1)
-      list.should.include('index.jade')
+    it('should know when filename is already correct.', function(done){
+      polymer.helpers.buildPriorityList('index.jade').should.eql(['index.jade'])
+      polymer.helpers.buildPriorityList('main.less').should.eql(['main.less'])
       done()
     })
 
@@ -80,36 +74,58 @@ describe("helpers", function(){
 
   describe('ingore', function(){
 
-    it('should ignore if starts with underscore', function(done){
+    it('should ignore if starts with underscore.', function(done){
       var reply = polymer.helpers.shouldIgnore('_beep.json')
       reply.should.be.true
       done()
     })
 
-    it('should not ignore if doesnt start with underscore', function(done){
+    it('should not ignore if doesnt start with underscore.', function(done){
       var reply = polymer.helpers.shouldIgnore('boop.json')
       reply.should.be.false
       done()
     })
 
-    it('should ignore if nested file starts with underscore', function(done){
+    it('should ignore if nested file starts with underscore.', function(done){
       var reply = polymer.helpers.shouldIgnore('beep/_boop.json')
       reply.should.be.true
       done()
     })
 
-    it('should ignore any part of tree starts with underscore', function(done){
+    it('should ignore any part of tree starts with underscore.', function(done){
       var reply = polymer.helpers.shouldIgnore('foo/_bar/baz.json')
       reply.should.be.true
       done()
     })
 
-    it('should allow underscore in names', function(done){
+    it('should allow underscore in names.', function(done){
       var reply = polymer.helpers.shouldIgnore('foo_/beep.json')
       reply.should.be.false
       done()
     })
 
+  })
+
+  describe('outputPath', function(){
+    it('should convert jade to html.', function(done){
+      polymer.helpers.outputPath('foobar.html').should.eql('foobar.html')
+      polymer.helpers.outputPath('foobar.jade').should.eql('foobar.html')
+      polymer.helpers.outputPath('foobar.html.jade').should.eql('foobar.html')
+      done()
+    })
+
+    it('should convert less to css.', function(done){
+      polymer.helpers.outputPath('foobar.css').should.eql('foobar.css')
+      polymer.helpers.outputPath('foobar.less').should.eql('foobar.css')
+      polymer.helpers.outputPath('foobar.css.less').should.eql('foobar.css')
+      done()
+    })
+
+    it('should allow alternate file extensions.', function(done){
+      polymer.helpers.outputPath('foobar.foo').should.eql('foobar.foo')
+      polymer.helpers.outputPath('foobar.bar.jade').should.eql('foobar.bar')
+      done()
+    })
   })
 
 })
