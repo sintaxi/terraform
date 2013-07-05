54c848d96eb043d48f5df583fc93769462427e0c
Brock Whitten <brock@chloi.io>
2013-07-05 03:29:48 -0700

    inital addition of ejs implimentation

--- test/jade.js
+++ test/jade.js
@@ -6,6 +6,18 @@ describe("jade", function(){
   var root = __dirname + "/fixtures/templates"
   var poly = polymer.root(root)
 
+  it("should render ejs file", function(done){
+    poly.render("bio.ejs", function(error, body){
+      console.log("---")
+      console.log(error)
+      console.log(body)
+      should.not.exist(error)
+      should.exist(body)
+      body.should.include("<h1>Hello EJS</h1>")
+      done()
+    })
+  })
+
   it("should render markdown file", function(done){
     poly.render("stuff.md", function(error, body){
       should.not.exist(error)
