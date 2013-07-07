09a1885ce923420cd1ba7a926430d68b591ceda7
Brock Whitten <brock@chloi.io>
2013-07-07 02:48:34 -0700

    first steps on stylus support

--- test/stylesheets.js
+++ test/stylesheets.js
@@ -10,7 +10,6 @@ describe("less", function(){
     poly.render("main.less", function(error, body){
       should.not.exist(error)
       body.should.include("body{background:pink;}")
-
       done()
     })
   })
@@ -25,5 +24,31 @@ describe("less", function(){
       done()
     })
   })
+
+})
+
+describe("stylus", function(){
+
+  var root = __dirname + '/fixtures/stylesheets/stylus'
+  var poly = polymer.root(root)
+
+  // it("should have basic css file", function(done){
+  //   poly.render("main.stylus", function(error, body){
+  //     should.not.exist(error)
+  //     body.should.include("body{background:pink;}")
+  //     done()
+  //   })
+  // })
+
+  // it("should return errors if error found", function(done){
+  //   poly.render("invalid.stylus", function(error, body){
+  //     should.not.exist(body)
+  //     should.exist(error)
+  //     error.should.have.property("name")
+  //     error.should.have.property("message")
+  //     error.should.have.property("stack")
+  //     done()
+  //   })
+  // })
 
 })
