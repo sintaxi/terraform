be9cb83fd51ea385d623a68f5a33b1d9a66801ed
Brock Whitten <brock@chloi.io>
2013-07-07 02:54:54 -0700

    fixed helpers

--- test/stylesheets.js
+++ test/stylesheets.js
@@ -1,54 +1,56 @@
 var should  = require('should')
 var polymer = require('../')
 
-describe("less", function(){
+describe("stylesheets", function(){
 
-  var root = __dirname + '/fixtures/stylesheets/less'
-  var poly = polymer.root(root)
+  describe(".less", function(){
 
-  it("should have basic css file", function(done){
-    poly.render("main.less", function(error, body){
-      should.not.exist(error)
-      body.should.include("body{background:pink;}")
-      done()
+    var root = __dirname + '/fixtures/stylesheets/less'
+    var poly = polymer.root(root)
+
+    it("should have basic css file", function(done){
+      poly.render("main.less", function(error, body){
+        should.not.exist(error)
+        body.should.include("body{background:pink;}")
+        done()
+      })
     })
+
+    it("should return errors if error found", function(done){
+      poly.render("invalid.less", function(error, body){
+        should.not.exist(body)
+        should.exist(error)
+        error.should.have.property("name")
+        error.should.have.property("message")
+        error.should.have.property("stack")
+        done()
+      })
+    })
+
   })
 
-  it("should return errors if error found", function(done){
-    poly.render("invalid.less", function(error, body){
-      should.not.exist(body)
-      should.exist(error)
-      error.should.have.property("name")
-      error.should.have.property("message")
-      error.should.have.property("stack")
-      done()
-    })
+  describe(".stylus", function(){
+
+    var root = __dirname + '/fixtures/stylesheets/stylus'
+    var poly = polymer.root(root)
+
+    // it("should have basic css file", function(done){
+    //   poly.render("main.stylus", function(error, body){
+    //     should.not.exist(error)
+    //     body.should.include("body{background:pink;}")
+    //     done()
+    //   })
+    // })
+
+    // it("should return errors if error found", function(done){
+    //   poly.render("invalid.stylus", function(error, body){
+    //     should.not.exist(body)
+    //     should.exist(error)
+    //     error.should.have.property("name")
+    //     error.should.have.property("message")
+    //     error.should.have.property("stack")
+    //     done()
+    //   })
+    // })
+
   })
-
-})
-
-describe("stylus", function(){
-
-  var root = __dirname + '/fixtures/stylesheets/stylus'
-  var poly = polymer.root(root)
-
-  // it("should have basic css file", function(done){
-  //   poly.render("main.stylus", function(error, body){
-  //     should.not.exist(error)
-  //     body.should.include("body{background:pink;}")
-  //     done()
-  //   })
-  // })
-
-  // it("should return errors if error found", function(done){
-  //   poly.render("invalid.stylus", function(error, body){
-  //     should.not.exist(body)
-  //     should.exist(error)
-  //     error.should.have.property("name")
-  //     error.should.have.property("message")
-  //     error.should.have.property("stack")
-  //     done()
-  //   })
-  // })
-
-})
