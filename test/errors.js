95e17c49d576947dd58cc445c14df9af8f412184
Brock Whitten <brock@chloi.io>
2013-07-13 16:17:17 -0700

    centralized error test fixtures

--- test/errors.js
+++ test/errors.js
@@ -6,37 +6,42 @@ describe("errors", function(){
   var root = __dirname + "/fixtures/errors"
   var poly = polymer.root(root)
 
-  it("should get errors obj from jade", function(done){
-    poly.render("novar.jade", function(error, body){
-      should.not.exist(body)
-      should.exist(error)
-      error.should.have.property('name')
-      error.should.have.property('message')
-      error.should.have.property('stack')
-      done()
+  describe(".less", function(){
+    it("should get error if var missing in less", function(done){
+      poly.render("less/novar.less", function(error, body){
+        should.not.exist(body)
+        should.exist(error)
+        error.should.have.property('name')
+        error.should.have.property('message')
+        error.should.have.property('stack')
+        done()
+      })
+    })
+
+    it("should get errors if syntax not correct", function(done){
+      poly.render("less/invalid.less", function(error, body){
+        should.not.exist(body)
+        should.exist(error)
+        error.should.have.property('name')
+        error.should.have.property('message')
+        error.should.have.property('stack')
+        done()
+      })
     })
   })
 
-  it("should get error if var missing in less", function(done){
-    poly.render("novar.less", function(error, body){
-      should.not.exist(body)
-      should.exist(error)
-      error.should.have.property('name')
-      error.should.have.property('message')
-      error.should.have.property('stack')
-      done()
+  describe(".jade", function(){
+    it("should get errors obj from jade", function(done){
+      poly.render("jade/novar.jade", function(error, body){
+        should.not.exist(body)
+        should.exist(error)
+        error.should.have.property('name')
+        error.should.have.property('message')
+        error.should.have.property('stack')
+        done()
+      })
     })
   })
 
-  it("should get errors if syntax not correct", function(done){
-    poly.render("invalid.less", function(error, body){
-      should.not.exist(body)
-      should.exist(error)
-      error.should.have.property('name')
-      error.should.have.property('message')
-      error.should.have.property('stack')
-      done()
-    })
-  })
 
 })
