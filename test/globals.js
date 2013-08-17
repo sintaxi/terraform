2bec3bde3ac83c959842a7f9c67a98c511656e4b
Brock Whitten <brock@chloi.io>
2013-08-16 23:33:40 -0700

    adds tests for globals change

---
+++ test/globals.js
@@ -0,0 +1,31 @@
+
+var should  = require('should')
+var polymer = require('../')
+
+describe("data", function(){
+
+  describe("valid", function(){
+    var root = __dirname + "/fixtures/globals"
+    var poly = polymer.root(root, { "title": "Default Title" })
+
+    it("should have global available by default", function(done){
+      poly.render("index.jade", function(error, body){
+        should.not.exist(error)
+        should.exist(body)
+        body.should.include("<title>Default Title</title>")
+        done()
+      })
+    })
+
+    it("should be able to override globals in the template vars", function(done){
+      poly.render("about.jade", function(error, body){
+        should.not.exist(error)
+        should.exist(body)
+        body.should.include("<title>About Page</title>")
+        done()
+      })
+    })
+
+  })
+
+})
