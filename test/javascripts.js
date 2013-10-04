dc826c21c16cbb11c97b9bd5a94877c96e056825
Audrey Tang <audreyt@audreyt.org>
2013-10-05 04:39:31 +0800

    * Add support for LiveScript: http://livescript.net/

--- test/javascripts.js
+++ test/javascripts.js
@@ -28,4 +28,30 @@ describe("javascripts", function(){
 
   })
 
+  describe(".ls", function(){
+    var root = __dirname + "/fixtures/javascripts/ls"
+    var poly = polymer.root(root)
+
+    it("should translate livescript to javascript", function(done){
+      poly.render("main.ls", function(errors, body){
+        should.not.exist(errors)
+        should.exist(body)
+        done()
+      })
+    })
+
+    it("should return errors if invalid", function(done){
+      poly.render("invalid.ls", function(errors, body){
+        should.exist(errors)
+        should.not.exist(body)
+        errors.should.have.property("name")
+        errors.should.have.property("message")
+        errors.should.have.property("stack")
+        done()
+      })
+    })
+
+  })
+
+
 })
