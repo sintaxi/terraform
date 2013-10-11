3abe8ec3be768bd8f937c1e811cf7e112af3a729
Brock Whitten <brock@chloi.io>
2013-10-11 01:03:36 -0700

    fixes jade test error

--- test/errors.js
+++ test/errors.js
@@ -98,7 +98,7 @@ describe("errors", function(){
         should.exist(error)
         error.should.have.property('source')
         error.should.have.property('dest')
-        error.should.have.property('lineno', -1)
+        error.should.have.property('lineno', 3)
         error.should.have.property('filename')
         error.should.have.property('message')
         error.should.have.property('stack')
