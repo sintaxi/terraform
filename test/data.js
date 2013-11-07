f4d26cdf5200cbdc74d8613360b61ab38c7e9d29
Brock Whitten <brock@chloi.io>
2013-11-07 02:25:22 -0800

    writes failing test for dot directory ignoring

--- test/data.js
+++ test/data.js
@@ -74,4 +74,16 @@ describe("data", function(){
     })
   })
 
+  describe("public", function(){
+    it("should return public object", function(done){
+      var root = __dirname + "/fixtures/data/valid"
+      var poly = polymer.root(root)
+      poly.render("pub.json.jade", { "layout": false }, function(err, result){
+        var pub = JSON.parse(result)
+        should.not.exist(pub[".foo"])
+        done()
+      })
+    })
+  })
+
 })
