da3dc363161283abf78542164fb516d733bafdab
kennethormandy <hello@kennethormandy.com>
2013-12-18 15:21:52 -0800

    Adds tests for rendering special characters

--- test/render.js
+++ test/render.js
@@ -133,4 +133,44 @@ describe("render(path, callback)", function(){
     })
   })
 
+  describe("internationalization", function(){
+
+    it("should render special characters in jade", function(done){
+      var root = __dirname + "/fixtures/render/internationalization"
+      var poly = polymer.root(root)
+      poly.render("jade.jade", function(errors, body){
+        should.not.exist(errors)
+        should.exist(body)
+        body.should.include("<h1>“Iñtërnâtiônàlizætiøn”</h1>")
+        body.should.include("</html>")
+        done()
+      })
+    })
+
+    it("should render special characters in ejs", function(done){
+      var root = __dirname + "/fixtures/render/internationalization"
+      var poly = polymer.root(root)
+      poly.render("ejs.ejs", function(errors, body){
+        should.not.exist(errors)
+        should.exist(body)
+        body.should.include("<h1>“Iñtërnâtiônàlizætiøn”</h1>")
+        body.should.include("</html>")
+        done()
+      })
+    })
+
+    it("should render special characters in markdown", function(done){
+      var root = __dirname + "/fixtures/render/internationalization"
+      var poly = polymer.root(root)
+      poly.render("markdown.md", function(errors, body){
+        should.not.exist(errors)
+        should.exist(body)
+        body.should.include("<h1>“Iñtërnâtiônàlizætiøn”</h1>")
+        body.should.include("</html>")
+        done()
+      })
+    })
+
+  })
+
 })
