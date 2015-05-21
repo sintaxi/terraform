edfc34545e5dc5bf1e2d8496f94079ffd9981aa4
Kenneth Ormandy <kenneth@chloi.io>
2015-05-21 16:17:31 -0700

    Adds passing tests for Handlebars’ use of global data

--- test/globals.js
+++ test/globals.js
@@ -6,15 +6,21 @@ describe("data", function(){
 
   describe("valid", function(){
     var root = __dirname + "/fixtures/globals"
-    var poly = polymer.root(root, { "title": "Default Title" })
+    var poly = polymer.root(root, { "title": "Default Title", "name": "Annie Person" })
 
     it("should have global available by default", function(done){
       poly.render("index.jade", function(error, body){
         should.not.exist(error)
         should.exist(body)
         body.should.include("<title>Default Title</title>")
+      })
+      poly.render("contact.hbs", function(error, body){
+        should.not.exist(error)
+        should.exist(body)
+        body.should.include("<h1>Annie Person</h1>")
         done()
       })
+
     })
 
     it("should be able to override globals in the template vars", function(done){
@@ -22,6 +28,11 @@ describe("data", function(){
         should.not.exist(error)
         should.exist(body)
         body.should.include("<title>About Page</title>")
+      })
+      poly.render("blog.hbs", function(error, body){
+        should.not.exist(error)
+        should.exist(body)
+        body.should.include("<h1>Blog Page</h1>")
         done()
       })
     })
