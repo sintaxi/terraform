b4b6c8d7eece94d1d33d4bac7ae0dc8576681bf8
Marios Antonoudiou <marios.ant@qboss.eu>
2016-01-28 20:59:59 +0000

    Add the boilerplate for partial() tag in nunjucks

--- test/render.js
+++ test/render.js
@@ -110,6 +110,19 @@ describe("render(path, callback)", function(){
         done()
       })
     })
+      
+    it("should have mixes partials with locals for nunjucks as well", function(done){
+      poly.render("index.nunjucks", function(error, body){
+        should.not.exist(error)
+        should.exist(body)
+        body.should.include("<h1>Hello</h1>")
+        body.should.include("<h2>Hello World</h2>")
+        body.should.include("<h2>Hello Brazil</h2>")
+        body.should.include("<h2>Hello Canada</h2>")
+        // body.should.include("<h2>Hello Gastown</h2>")
+        done()
+      })
+    })
 
     it("should not render file with underscore", function(done){
       poly.render("_places/brazil.jade", function(error, body){
